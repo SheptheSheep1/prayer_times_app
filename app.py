fa814e31dddf64e1e103b1f2cf78a11d6e5e20aa
Ibrahim Safi <35405069+SheptheSheep1@users.noreply.github.com>
2024-07-25 16:44:25 +0430

    utc offset and some basic structure

--- app.py
+++ app.py
@@ -1,9 +1,18 @@
 import time
-def main():
-    print("Hello World")
-    target_host = "api.aladhan.com"
-    print(time.localtime().tm_gmtoff/60/60)
+from datetime import datetime, timezone
 
+def main():
+    #print(time.localtime().tm_gmtoff/60/60)
+    #print(datetime.now(timezone.utc).astimezone())
+    ts = time.time()
+    utc_offset = ((datetime.fromtimestamp(ts).timestamp()) - datetime.fromtimestamp(ts, timezone.utc).replace(tzinfo=None).timestamp())/3600.0
+    print(utc_offset)
+
+#def convertTimeDec(time: int) 
+class PrayerTime:
+    def __init__(self, month, day, year, hour, minute):
+        pass
+    
 
 if __name__ == "__main__":
     main()
