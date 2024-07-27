ab44f4ad1107a08dce1372225b21081a3b22130d
Ibrahim Safi <35405069+SheptheSheep1@users.noreply.github.com>
2024-07-27 07:46:06 +0430

    valid julian day algo added

--- app.py
+++ app.py
@@ -1,12 +1,18 @@
-from collections import namedtuple
+import math
+import os
 import time
 from datetime import datetime, timezone
+from collections import namedtuple
+
 from geopy.geocoders import Nominatim
 
 def main():
+    print(date_to_jd(2024, 7, 27))
+    zoop = datetime(2024, 7, 27, 7, 38, 0)
+    print(julian_day(zoop))
     obj = PrayerTime()
-    #print(time.localtime().tm_gmtoff/60/60)
-    #print(datetime.now(timezone.utc).astimezone())
+    print(obj.calcJulianDays(2024, 7, 27, 7, 0, 0))
+    # print(os.environ['LATITUDE'])
     print("\n-------------------------------------")
     print("-------------------------------------")
     print("----------Welcome to Ma'ruf----------")
@@ -20,10 +26,10 @@ def main():
         day = int(input("Enter the day of the month (format: '09'): ").strip())
         utc_timezone = float(input("Enter your timezone's offset from UTC (format: '11.5'): ").strip())
         prayerTime = PrayerTime(month, day, year, utc_timezone)
-    if getYesNo("Ma'ruf requires GPS latitude and longitude coordinates in order to calculate prayer times\nWould you like to use an approximation of your GPS coordinates based on your public IPv4 address? (requires an active internet connection)"):
+    if getYesNo("\nMa'ruf requires GPS latitude and longitude coordinates in order to calculate prayer times\nWould you like to use an approximation of your GPS coordinates based on your public IPv4 address? (requires an active internet connection)"):
         pass
-    elif getYesNo("Would you like to use an approximation based on your city? (requires an active internet connection, uses Nominatim API)"):
-        city = str(input(("Enter your city/country (format: New York, USA)")))
+    elif getYesNo("\nWould you like to use an approximation based on a given city? (requires an active internet connection, uses Nominatim API)"):
+        city = str(input(("Enter your city/country (format: New York, USA): ")))
         prayerTime.setCoordsbyCity(city)
     else:
         try:
@@ -32,6 +38,68 @@ def main():
             prayerTime.setGPScoordinates(latitude, longitude)
         except ValueError:
             print("Please enter a number in the given format")
+
+def date_to_jd(year,month,day):
+    """
+    Convert a date to Julian Day.
+    
+    Algorithm from 'Practical Astronomy with your Calculator or Spreadsheet', 
+        4th ed., Duffet-Smith and Zwart, 2011.
+    
+    Parameters
+    ----------
+    year : int
+        Year as integer. Years preceding 1 A.D. should be 0 or negative.
+        The year before 1 A.D. is 0, 10 B.C. is year -9.
+        
+    month : int
+        Month as integer, Jan = 1, Feb. = 2, etc.
+    
+    day : float
+        Day, may contain fractional part.
+    
+    Returns
+    -------
+    jd : float
+        Julian Day
+        
+    Examples
+    --------
+    Convert 6 a.m., February 17, 1985 to Julian Day
+    
+    >>> date_to_jd(1985,2,17.25)
+    2446113.75
+    
+    """
+    if month == 1 or month == 2:
+        yearp = year - 1
+        monthp = month + 12
+    else:
+        yearp = year
+        monthp = month
+    
+    # this checks where we are in relation to October 15, 1582, the beginning
+    # of the Gregorian calendar.
+    if ((year < 1582) or
+        (year == 1582 and month < 10) or
+        (year == 1582 and month == 10 and day < 15)):
+        # before start of Gregorian calendar
+        B = 0
+    else:
+        # after start of Gregorian calendar
+        A = math.trunc(yearp / 100.)
+        B = 2 - A + math.trunc(A / 4.)
+        
+    if yearp < 0:
+        C = math.trunc((365.25 * yearp) - 0.75)
+    else:
+        C = math.trunc(365.25 * yearp)
+        
+    D = math.trunc(30.6001 * (monthp + 1))
+    
+    jd = B + C + D + day + 1720994.5
+    
+    return jd
 
 def getYesNo(question: str) -> bool:
     while True:
@@ -46,6 +114,7 @@ def getYesNo(question: str) -> bool:
 def getLocalUTCOffset(time) -> float:
     return ((datetime.fromtimestamp(time).timestamp()) - datetime.fromtimestamp(time, timezone.utc).replace(tzinfo=None).timestamp())/3600.0
 
+
 class PrayerTime:
     __ts = time.time()
     __month = 0
@@ -54,7 +123,10 @@ class PrayerTime:
     __utc_offset = 0.0
     __latitude = 0.0
     __longitude = 0.0
-    geolocator = Nominatim(user_agent='maruf')
+    __geolocator = Nominatim(user_agent='maruf')
+    __FAJR_ANGLE = 0.0
+    __ISHA_ANGLE = 0.0
+    CALCULATION_METHOD = 0
 
     def __init__(self, month=datetime.now().date().month, day=datetime.now().date().day, year=datetime.now().date().day, utc_offset=getLocalUTCOffset(time.time())):
         self.__month = month
@@ -68,9 +140,90 @@ class PrayerTime:
     
     def setCoordsbyCity(self, city: str):
         location = self.geolocator.geocode(city)
-        latitude = location.latitude
+        self.__latitude = location.latitude
         longitude = location.longitude
-        print(latitude, longitude)
+
+    def setFajrIshaAngles(self, fajr_angle: float, isha_angle: float):
+        self.__FAJR_ANGLE = fajr_angle
+        self.__ISHA_ANGLE = isha_angle
+
+    def promptCalcMethod(self):
+        CalcMethod = namedtuple("CalcMethod", ["name", "fajr_angle", "isha_angle", "fixed"])
+        print(f'''
+              (1) MWL (Muslim World League) Fajr: 18\N{DEGREE SIGN} Isha: 17\N{DEGREE SIGN}
+              (2) ISNA (Islamic Society of North America) Fajr: 15\N{DEGREE SIGN} Isha: 15\N{DEGREE SIGN}
+              (3) Umm al-Qura (Umm al-Qura University, Makkah) Fajr: 18.5\N{DEGREE SIGN} Isha: 90 mins after Maghrib, 120 mins during Ramadan
+              (4) Gulf Fajr: 19.5\N{DEGREE SIGN} Isha: 90 mins after Maghrib, 120 mins during Ramadan
+              (5) Algerian (Algerian Ministry of Religious Affairs and Wakfs) Fajr: 18\N{DEGREE SIGN} Isha: 17\N{DEGREE SIGN}
+              (6) Karachi (University of Islamic Sciences, Karachi) Fajr: 18\N{DEGREE SIGN} Isha: 18\N{DEGREE SIGN}
+              (7) Diyanet (Diyanet İşleri Başkanlığı, Turkey) Fajr: 18\N{DEGREE SIGN} Isha: 17\N{DEGREE SIGN}
+              (8) Egypt (Egyptian General Authority of Survey) Fajr: 19.5\N{DEGREE SIGN} Isha: 17.5\N{DEGREE SIGN}
+              (9) EgyptBis (Egyptian General Authority of Survey) Fajr: 20\N{DEGREE SIGN} Isha: 18\N{DEGREE SIGN}
+              (10) Kemenag (Kementerian Agama Republik Indonesia) Fajr: 20\N{DEGREE SIGN} Isha: 18\N{DEGREE SIGN}
+              (11) MUIS (Majlis Ugama Islam Singapura) Fajr: 20\N{DEGREE SIGN} Isha: 18\N{DEGREE SIGN}
+              (12) JAKIM (Jabatan Kemajuan Islam Malaysia) Fajr: 20\N{DEGREE SIGN} Isha: 18\N{DEGREE SIGN}
+              (13) UDIF (Union Des Organisations Islamiques De France) Fajr: 12\N{DEGREE SIGN} Isha: 12\N{DEGREE SIGN}
+              (14) France15 Fajr: 15\N{DEGREE SIGN} Isha: 15\N{DEGREE SIGN}
+              (15) France18 Fajr: 18\N{DEGREE SIGN} Isha: 18\N{DEGREE SIGN}
+              (16) Tunisia (Tunisian Ministry of Religious Affairs) Fajr: 18\N{DEGREE SIGN} Isha: 18\N{DEGREE SIGN}
+              (17) Tehran (Institute of Geophysics, University of Tehran) Fajr: 17.7\N{DEGREE SIGN} Isha: 14\N{DEGREE SIGN}
+              (18) Jafari (Shia Ithna Ashari) Fajr: 16\N{DEGREE SIGN} Isha: 14\N{DEGREE SIGN}
+              ''')
+        answer = input("\nChoose your calculation method: ").strip()
+        match answer:
+            case 1:
+                CALCULATION_METHOD = CalcMethod("MWL", 18.0, 17.0, False)
+            case 2:
+                CALCULATION_METHOD = CalcMethod("ISNA", 15.0, 15.0, False)
+            case 3:
+                CALCULATION_METHOD = CalcMethod("Umm al-Qura", 18.5, 90, True)
+            case 4:
+                CALCULATION_METHOD = CalcMethod("Gulf", 19.5, 90, True)
+            case 5:
+                CALCULATION_METHOD = CalcMethod("Algerian", 18, 17, False)
+            case 6:
+                CALCULATION_METHOD = CalcMethod("Karachi", 18, 18, False)
+            case 7:
+                CALCULATION_METHOD = CalcMethod("Diyanet", 18, 17, False)
+            case 8:
+                CALCULATION_METHOD = CalcMethod("Egypt", 19.5, 17.5, False)
+            case 9:
+                CALCULATION_METHOD = CalcMethod("EgyptBis", 20.0, 18.0, False)
+            case 10:
+                CALCULATION_METHOD = CalcMethod("Kemenag", 20.0, 18.0, False)
+            case 11:
+                CALCULATION_METHOD = CalcMethod("MUIS", 20.0, 18.0, False)
+            case 12: 
+                CALCULATION_METHOD = CalcMethod("JAKIM", 20.0, 18.0, False)
+            case 13:
+                CALCULATION_METHOD = CalcMethod("UDIF", 12.0, 12.0, False)
+            case 14:
+                CALCULATION_METHOD = CalcMethod("France15", 15.0, 15.0, False)
+            case 15:
+                CALCULATION_METHOD = CalcMethod("France18", 18.0, 18.0, False)
+            case 16:
+                CALCULATION_METHOD = CalcMethod("Tunisia", 18.0, 18.0, False)
+            case 17:
+                CALCULATION_METHOD = CalcMethod("Tehran", 17.7, 14.0, False)
+            case 18:
+                CALCULATION_METHOD = CalcMethod("Jafari", 16.0, 14.0, False)
+            case _:
+                CALCULATION_METHOD = CalcMethod("MWL", 18.0, 17.0, False)
+        print(f"{CALCULATION_METHOD.name} chosen")
+# Garbagio JD algo
+'''
+    def calcJulianDays(self, year: int, month: int, day: int, hour: int, minute: int, second: int) -> float:
+        if (month <= 2):
+            month = month + 12
+            year = year - 1
+        B = 0
+        if month > 1582 and month > 10 and day > 14:
+            A = math.floor(year / 100)
+            B = 2 + math.floor(A / 4) - A
+        julian = 367 * year - int((7*(year + int((month + 9) /12.0))) + 4.0) + int((275*month)/9.0) + day + 1721013.5 + (hour + (minute/60.0) + second / math.pow(60, 2)) / 24.0 - 0.5 * math.copysign(1, 100 * year + month - 190002.5) + 0.5
+        #return (1720994.5 + math.floor(365.25*year) + math.floor(30.6001*(month+1)) + B + day + (((hour * 3600) + (month*60) + second) / 86400) - (self.__utc_offset / 24))
+        return julian
+'''
 
 if __name__ == "__main__":
     main()
